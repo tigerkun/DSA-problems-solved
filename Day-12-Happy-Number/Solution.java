0658b92b14118b6da1d414f84711f76f9e17c5b0
tigerkun <tejasgaur94@gmail.com>
2026-06-11 10:12:59 +0530

    solution

---
+++ Day-12-Happy-Number/Solution.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isHappy(int n) {
+        int slow = n;
+        int fast = n;
+
+        do {
+            slow = getNext(slow);
+            fast = getNext(getNext(fast));
+        } while (slow != fast);
+
+        return slow == 1;
+    }
+
+    private int getNext(int num) {
+        int sum = 0;
+
+        while (num > 0) {
+            int digit = num % 10;
+            sum += digit * digit;
+            num /= 10;
+        }
+
+        return sum;
+    }
+}
