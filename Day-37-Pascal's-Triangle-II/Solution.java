70cf8362edfc1f3ccc1140d519b43474a6322df2
tigerkun <tejasgaur94@gmail.com>
2026-07-07 00:25:52 +0530

    created solution

---
+++ Day-37-Pascal's-Triangle-II/Solution.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Solution {
+    public List<Integer> getRow(int rowIndex) {
+        List<Integer> row = new ArrayList<>();
+
+        long value = 1;
+
+        for (int i = 0; i <= rowIndex; i++) {
+            row.add((int) value);
+            value = value * (rowIndex - i) / (i + 1);
+        }
+
+        return row;
+    }
+}
